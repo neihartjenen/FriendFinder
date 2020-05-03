352cc66bbd5bbf2e719e6efa1823907cf46eb85b
jenenneihart <neihart.jenen@gmail.com>
2020-05-02 21:13:29 -0700

    testing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,6 +8,13 @@ app.get("/api/friends", function(req,res){
 })
 
 app.post('/api/friends', function(req, res) {
+
     var userInput = req.body;
-    var userAnswer = userInput.scores
+    var userAnswer = userInput.scores;
     var matchName = '';
+    var matchEmail = '';
+   
+   
+   
+   
+    friends.push(userInput);
