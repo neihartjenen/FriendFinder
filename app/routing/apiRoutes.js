6cbc417643db0f7ca19d3583b810273d09f8f090
jenenneihart <neihart.jenen@gmail.com>
2020-07-26 23:04:20 -0700

    added the onclick function and ajax call to friends api, app is working and g2g

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,6 @@
 var friends = require("../data/friends");
 
 module.exports = function(app) {
-
-// Displays all friends
 app.get("/data/friends", function(req, res) {
     return res.json(friends);
   });
@@ -15,22 +13,23 @@ app.post("/data/friends", function(req, res) {
     newScores = newSurvey.scores,
     scoreDifference;
     
-    for(var f = 0; f < friends[i].scores[f]; f++) {
-        console.log(friends[i].name);
-        scoreDifference = 0;
-        for(var f = 0; f < friends[i].scores[f]; f++) {
-            scoreDifference += Math.abs(parseInt(newScores[f]) - parseInt(friends[i].scores[f]));
-            if(scoreDifference <= bestMatch.difference) {
-              bestMatch.name = friends[i].name;
-              bestMatch.photo = friends[i].photo;
-              bestMatch.difference = scoreDifference;
-            }
-          }
+    // Loop through friends data
+    for(var i = 0; i < friends.length; i++) {
+      console.log(friends[i].name);
+      scoreDifference = 0;
+
+      // Loop through scores
+      for(var f = 0; f < friends[i].scores[f]; f++) {
+        scoreDifference += Math.abs(parseInt(newScores[f]) - parseInt(friends[i].scores[f]));
+        if(scoreDifference <= bestMatch.difference) {
+          bestMatch.name = friends[i].name;
+          bestMatch.photo = friends[i].photo;
+          bestMatch.difference = scoreDifference;
         }
-    
-        // Pushs survey data to friends
-        friends.push(newSurvey);
-        res.json(bestMatch);
-      });
-    };
-        
+      }
+    }
+    // Push new survey data to friends
+    friends.push(newSurvey);
+    res.json(bestMatch);
+  });
+};
